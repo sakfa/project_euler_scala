4fd51e3d36654b9a89e7f918bdb3ebe685b1fe33
Mariusz Sakowski <mariusz.sakowski@fastwhitecat.com>
2013-05-16 17:33:47 +0200

    p51

--- src/solutions/Problem1.scala
+++ src/solutions/Problem1.scala
@@ -1,11 +1,15 @@
 package solutions
 
+import tools.TimeTool
+
 object Problem1 {
   def main(args: Array[String]) {
-    val result = (1 until 1000).map { (i:Int) =>
-      if (i % 3 == 0 || i % 5 == 0) i else 0
-    }.sum
+    TimeTool.start
+
+    val result = (1 until 1000).filter(i => i % 3 == 0 || i % 5 == 0).sum
 
     println(result)
+
+    TimeTool.printAndStop
   }
 }
